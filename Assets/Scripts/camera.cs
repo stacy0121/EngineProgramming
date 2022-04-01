771d6574fd3455d07708cd19498e5829e1350709
stacy0121 <stcie12@gmail.com>
2022-04-01 12:32:35 +0900

    0401

---
+++ Assets/Scripts/camera.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    public Transform target;
+    private Transform tr;
+
+    public float offsetX;
+    public float offsetY;
+    public float offsetZ;
+
+    public float DelayTime;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 FixedPos = new Vector3(target.position.x + offsetX, target.position.y + offsetY, target.position.z + offsetZ);
+        transform.position = Vector3.Lerp(transform.position, FixedPos, Time.deltaTime * DelayTime);
+    }
+    private void LateUpdate()
+    {
+        tr.position = new Vector3(target.position.x -0.52f, tr.position.y, target.position.z - 20);
+
+        tr.LookAt(target);
+    }
+}
