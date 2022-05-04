c2f8750214b2e12641258fb89d0f1775b2a80278
stacy012 <72933504+stacy0121@users.noreply.github.com>
2022-05-04 14:48:38 +0900

    0504

---
+++ topDown_Car/Assets/Scripts/drive.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class drive : MonoBehaviour
+{
+    public Rigidbody2D rb2d;
+    float xValue;
+    float yValue;
+    public float speed;   // 가속력
+    public Camera camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        yValue = Input.GetAxis("Vertical");
+        xValue = Input.GetAxis("Horizontal");
+
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = 7;
+        }
+
+        camera.transform.position = new Vector3(transform.position.x, transform.position.y, -10);
+    }
+
+    public void FixedUpdate()   // 물리엔진
+    {
+        //rb2d.velocity = new Vector2(0, yValue);   // 좌표 변경
+        rb2d.velocity = transform.up*yValue*speed;   // 자기자신 곱하기 up
+        transform.Rotate(0, 0, -1 * xValue * 1);
+    }
+}
